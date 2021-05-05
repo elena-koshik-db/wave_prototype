35a946db2d7ffb58fb4e988386a01a14fdb428f1
Elena Koshik <elena.koshik@db.com>
2021-05-05 20:34:59 +0300

    remove usage of result of template creation

--- word-count-beam/src/main/java/org/apache/beam/examples/StreamingWordCount.java
+++ word-count-beam/src/main/java/org/apache/beam/examples/StreamingWordCount.java
@@ -2,7 +2,6 @@ package org.apache.beam.examples;
 
 import org.apache.beam.examples.common.WriteOneFilePerWindow;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
@@ -13,8 +12,6 @@ import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.joda.time.Duration;
-
-import java.io.IOException;
 
 
 public class StreamingWordCount {
@@ -35,7 +32,7 @@ public class StreamingWordCount {
         void setOutput(ValueProvider<String> value);*/
     }
 
-    static void runStreamingWordCount(Options options) throws IOException {
+    static void runStreamingWordCount(Options options) {
         options.setStreaming(true);
 
         Pipeline pipeline = Pipeline.create(options);
@@ -47,15 +44,10 @@ public class StreamingWordCount {
                 .apply(MapElements.via(new WordCount.FormatAsTextFn()))
                 .apply("Write Files to GCS", new WriteOneFilePerWindow(BUCKET_PATH, NUM_SHARDS));
 
-        PipelineResult result = pipeline.run();
-        try {
-            result.waitUntilFinish();
-        } catch (Exception exc) {
-            result.cancel();
-        }
+        pipeline.run();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
 
         runStreamingWordCount(options);
