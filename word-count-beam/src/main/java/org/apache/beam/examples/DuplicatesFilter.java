c0c7951aaf60e303a04782007ee1cc203dd85d5d
Elena Koshik <elena.koshik@db.com>
2021-07-23 21:09:51 +0300

    try with state, without timer

--- word-count-beam/src/main/java/org/apache/beam/examples/DuplicatesFilter.java
+++ word-count-beam/src/main/java/org/apache/beam/examples/DuplicatesFilter.java
@@ -1,13 +1,17 @@
 package org.apache.beam.examples;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.BooleanCoder;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.StreamingOptions;
-import org.apache.beam.sdk.transforms.Deduplicate;
+import org.apache.beam.sdk.state.StateSpec;
+import org.apache.beam.sdk.state.StateSpecs;
+import org.apache.beam.sdk.state.ValueState;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.ToString;
-import org.joda.time.Duration;
 
 
 public class DuplicatesFilter {
@@ -28,11 +32,39 @@ public class DuplicatesFilter {
 
         pipeline
                 .apply("Read PubSub messages", PubsubIO.readStrings().fromTopic(options.getInputTopic()))
-                .apply(Deduplicate.<String>values().withDuration(Duration.standardMinutes(WINDOW_SIZE_MIN)))
+                .apply(ParDo.of(new DeduplicateFn()))
+//                .apply(Deduplicate.<String>values().withDuration(Duration.standardMinutes(WINDOW_SIZE_MIN)))
                 .apply(ToString.elements())
                 .apply(PubsubIO.writeStrings().to("projects/sandbox-307310/topics/filter-out"));
 
         pipeline.run();
+    }
+
+    private static class DeduplicateFn extends DoFn<String, String> {
+        private static final String EXPIRY_TIMER = "expiryTimer";
+        private static final String SEEN_STATE = "seen";
+
+//        @TimerId(EXPIRY_TIMER)
+//        private final TimerSpec expiryTimerSpec;
+
+        @StateId(SEEN_STATE)
+        private final StateSpec<ValueState<Boolean>> seenState = StateSpecs.value(BooleanCoder.of());
+
+        @ProcessElement
+        public void processElement(
+                @Element String element,
+                OutputReceiver<String> receiver,
+                @StateId(SEEN_STATE) ValueState<Boolean> seenState) {
+//                @TimerId(EXPIRY_TIMER) Timer expiryTimer) {
+            Boolean seen = seenState.read();
+            // Seen state is either set or not set so if it has been set then it must be true.
+            if (seen == null) {
+                seenState.write(true);
+                receiver.output(element);
+            } else {
+                receiver.output(element + "~~~");
+            }
+        }
     }
 
     public static void main(String[] args) {
